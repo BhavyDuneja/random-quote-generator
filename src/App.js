e721ae1dca7b7526c4a68ebb418b3df2daecd7ab
Bhavy Duneja <bhavya.01714813120@ite.mait.ac.in>
2023-04-18 14:51:00 +0530

    Changes to be committed:phase 1 Application 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/images/bg.jpg 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { advice: ''};
+
+  componentDidMount(){
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then((response) => {
+        const {advice} = response.data.slip;
+         this.setState({advice});
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+  }
+  
+
+  render() {
+    const {advice} = this.state;
+    return(
+      <div className="App">
+        <div className="Card">
+            <div className="Heading">
+                <h1>{advice}</h1>
+            </div>
+            <button className='button' onClick={this.fetchAdvice()}>
+                <span>Give Me Advice!</span>
+            </button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
